04cec448920867ba18da610f79edd55b7ee6443d
C2SDK <C2SDK@C47427>
2021-04-15 18:26:43 -0700

    2DArraysIntroDone

--- src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
+++ src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
@@ -9,14 +9,19 @@ public class _01_IntroTo2DArrays {
         // 2. use two for loops to set all the elements in the 2D array to
         //    a different integer
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-    	
+    	int numbers = 0;
+    	for (int i = 0; i < array2D.length; i++) {
+			for (int j = 0; j < array2D.length; j++) {
+				array2D[i][j] = numbers++;
+			}
+		}
         
         // 3. print the third element from the second 1D array (2DArray[1][2])
-
+    	System.out.println(array2D[1][2]);
         // 4. set the third element from the second 1D array to a different
         //    value
-
+    	array2D[1][2] = 5;
         // 5. print the element again and verify the value has changed
-
+    	System.out.println(array2D[1][2]);
     }
 }
